13decc8706e9c6c96ae7919b5668e3f33dda05da
beka-tech <abeka039@gmail.com>
2025-01-27 14:03:29 +0300

    Update LogBetween2.js
    add For loop and While Loop

--- JavaScript Fundamentals/Control Flow/LogBetween2.js
+++ JavaScript Fundamentals/Control Flow/LogBetween2.js
@@ -1,8 +1,31 @@
 function logBetween(num1, num2) {
-  //let i = num1;
-
-  for (let i = num1; i <= num2; i++) {
-    console.log(i);
+  let i = num1;
+  if (num1 < num2) {
+    while (i <= num2) {
+      console.log(i);
+      i++;
+    }
+  } else {
+    console.log("Error");
   }
 }
-console.log(logBetween(-1, 2));
+
+// Examples:
+logBetween(-1, 2); // prints out:
+// -1
+// 0
+// 1
+// 2
+
+logBetween(14, 6); // => prints nothing
+
+function logBetween2(num1, num2) {
+  for (i = num1; i <= num2; i++) {
+    console.log(i + "  Using For Loop");
+  }
+}
+
+logBetween2(4, 6); // prints out:
+// 4
+// 5
+// 6
