28332a52372e6748d7ff1a42a931cb34b3a4e5e9
beka-tech <abeka039@gmail.com>
2025-06-04 13:44:56 +0300

    Triplet True
    tripletTrue that accepts a string as an argument. The function should return a boolean indicating whether or not the the string contains three of the same character consecutively.

--- JavaScript Fundamentals/String_Loops/Triplet_True.js
+++ JavaScript Fundamentals/String_Loops/Triplet_True.js
@@ -1,4 +1,14 @@
-// Your code here
+function tripletTrue(word) {
+  for (let i = 0; i < word.length; i++) {
+    let char1 = word[i];
+    let char2 = word[i + 1];
+    let char3 = word[i + 2];
+    if (char1 === char2 && char2 === char3) {
+      return true;
+    }
+  }
+  return false;
+}
 
 console.log(tripletTrue("caaabb")); // true
 console.log(tripletTrue("terrrrrible")); // true
