545aee5ee99b7f60f25bb5153aa4e7a957c632d5
beka-tech <abeka039@gmail.com>
2025-06-11 19:21:47 +0300

    update  Array_Index_Of

--- JavaScript Fundamentals/Intermediate_JavaScript_Array/Array_Index_Of.js
+++ JavaScript Fundamentals/Intermediate_JavaScript_Array/Array_Index_Of.js
@@ -11,11 +11,8 @@ function wordWithinArray(array, word) {
 console.log(
   wordWithinArray(["apple", "banana", "caramel", "chocolate"], "apple")
 ); //=> true
-
 console.log(wordWithinArray(["dog", "cat", "camel", "bird"], "camel")); //=> true
-
 console.log(
   wordWithinArray(["apple", "banana", "caramel", "chocolate"], "pineapple")
 ); //=> false
-
 console.log(wordWithinArray(["dog", "cat", "camel", "bird"], "panther")); //=> false
