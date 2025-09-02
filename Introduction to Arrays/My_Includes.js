063c57fc7617566edf0f2ce318a8a32dd95af23e
beka-tech <abeka039@gmail.com>
2025-09-02 13:42:46 +0300

    My Includes
    Write a function myIncludes(arr, target) that accepts an array and a target value as args. The function should return a boolean indicating whether the target is found in the array. Solve this without Array.includes or Array.indexOf.

---
+++ Introduction to Arrays/My_Includes.js
@@ -0,0 +1,16 @@
+// your code here
+function myIncludes(arr, target) {
+  for (let i = 0; i < arr.length; i++) {
+    let char = arr[i];
+    if (char === target) {
+      return true;
+    }
+  }
+  return false;
+}
+
+console.log(myIncludes(["a", "b", "c", "e"], "c")); // true
+console.log(myIncludes(["a", "b", "c", "e"], "a")); // true
+console.log(myIncludes(["a", "b", "c", "e"], "z")); // false
+console.log(myIncludes([43, -7, 11, 13], 11)); // true
+console.log(myIncludes([43, -7, 11, 13], 1)); // false
