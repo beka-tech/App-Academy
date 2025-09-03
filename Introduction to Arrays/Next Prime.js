88e96c1a0804253c08f5690e4acfc89eeccb8fab
beka-tech <abeka039@gmail.com>
2025-09-03 10:18:08 +0300

    Next Prime
    Write a function nextPrime that accepts a number as an argument. The function should return the nearest prime number that is greater than the given number.

---
+++ Introduction to Arrays/Next Prime.js
@@ -0,0 +1,25 @@
+// Your code here
+
+let isPrime = function (nums) {
+  let i = 2;
+
+  while (nums < i) {
+    nums++;
+  }
+};
+
+function nextPrime(nums) {
+  let Prime = [];
+
+  if (isPrime(nums)) {
+    Prime.push(num);
+  }
+  return Prime;
+}
+
+console.log(nextPrime(2)); // 3
+console.log(nextPrime(3)); // 5
+console.log(nextPrime(7)); // 11
+console.log(nextPrime(8)); // 11
+console.log(nextPrime(20)); // 23
+console.log(nextPrime(97)); // 101
