f50a25cbadbf9e7c68860225f071dabdcb9b834e
beka-tech <abeka039@gmail.com>
2025-09-17 11:46:22 +0300

    Fibonacci Sequence
    Write a function fibonacciSequence that accepts a number as an argument. The function should return an array representing the fibonacci sequence up to the given length. The first and second numbers of the sequence are 1 and 1. To generate subsequent numbers of the sequence, we take the sum of the previous two numbers of the sequence.

---
+++ Intermediate Arrays/Add to Arrays/Fibonacci_Sequence.js
@@ -0,0 +1,20 @@
+// Your code here
+
+let fibonacciSequence = function (nums) {
+  if (nums <= 0) return [];
+  if (nums === 1) return [1];
+
+  let seq = [1, 1];
+
+  for (let i = 2; i < nums; i++) {
+    seq.push(seq[i - 1] + seq[i - 2]);
+  }
+  return seq;
+};
+
+console.log(fibonacciSequence(4)); // [ 1, 1, 2, 3 ]
+console.log(fibonacciSequence(5)); // [ 1, 1, 2, 3, 5 ]
+console.log(fibonacciSequence(8)); // [ 1, 1, 2, 3, 5, 8, 13, 21 ]
+console.log(fibonacciSequence(0)); // [ ]
+console.log(fibonacciSequence(1)); // [ 1 ]
+console.log(fibonacciSequence(2)); // [ 1, 1 ]
