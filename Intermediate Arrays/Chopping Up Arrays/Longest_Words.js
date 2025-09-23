68bb9474ffdabeafbcd8e64f5763da5d93e81962
beka-tech <abeka039@gmail.com>
2025-09-23 14:30:05 +0300

    Summery
    Summary

---
+++ Intermediate Arrays/Chopping Up Arrays/Longest_Words.js
@@ -0,0 +1,17 @@
+// your code here
+let longestWord = function (Sentence) {
+  let words = Sentence.split(" ");
+
+  let longestWord = words[0];
+  for (let i = 0; i < words.length; i++) {
+    let word = words[i];
+    if (word.length > longestWord.length) {
+      longestWord = word;
+    }
+  }
+  return longestWord;
+};
+
+console.log(longestWord("where did everyone go")); // 'everyone'
+console.log(longestWord("prefer simplicity over complexity")); // 'simplicity'
+console.log(longestWord("")); // ''
