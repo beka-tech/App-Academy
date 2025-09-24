e88406fc776878a97865d91aa2466bebf689042b
beka-tech <abeka039@gmail.com>
2025-09-24 12:39:37 +0300

    Abbreviate Words
    Write a function abbreviateWords(sentence) that takes in a sentence string. The function should return a new sentence where words that are longer than 4 characters have their vowels removed. Hint: Consider creating a helper function to remove all vowels in a string.

---
+++ Intermediate Arrays/Chopping Up Arrays/Abbreviate_Words.js
@@ -0,0 +1,33 @@
+let removVowels = function (word) {
+  let vowels = "aeiou";
+  let newWord = " ";
+
+  for (let i = 0; i < word.length; i++) {
+    let char = word[i];
+
+    if (!vowels.includes(char)) {
+      newWord += char;
+    } else {
+      newWord;
+    }
+  }
+  return newWord;
+};
+
+let abbreviateWords = function (Sentence) {
+  let words = Sentence.split(" ");
+  let newWords = [];
+
+  for (let i = 0; i < words.length; i++) {
+    let word = words[i];
+    if (word.length > 4) {
+      newWords.push(removVowels(word));
+    } else {
+      newWords.push(word);
+    }
+  }
+  return newWords.join(" ");
+};
+
+console.log(abbreviateWords("what a wonderful place to live")); // what a wndrfl plc to live
+console.log(abbreviateWords("she sends an excellent message ")); // she snds an xcllnt mssg
