68cd24d66a7cfd4a778c22b6b27094453b45b523
beka-tech <abeka039@gmail.com>
2025-10-05 06:28:33 +0300

    Two Dimensional Product
    Write a function twoDimensionalProduct(array) that takes in a 2D array of numbers as an argument. The function should return the total product of all numbers multiplied together.

---
+++ Advanced Arrays/2D Array/Two_Dimensional_Product.js
@@ -0,0 +1,18 @@
+// your code here
+let twoDimensionalProduct = function (arr) {
+  let product = 1;
+
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr[i].length; j++) {
+      product *= arr[i][j];
+    }
+  }
+
+  return product;
+};
+
+let arr1 = [[6, 4], [5], [3, 1]];
+console.log(twoDimensionalProduct(arr1)); // 360
+
+let arr2 = [[11, 4], [2]];
+console.log(twoDimensionalProduct(arr2)); // 88
